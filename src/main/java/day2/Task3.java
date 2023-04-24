d4c5402399ab624b9cdd06ec0353e2243287bc99
dzhon_at_work <evgenroo@gmail.com>
2023-04-24 16:35:22 +0300

    day2 done

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
+// Реализовать программу №2, используя цикл while.
 public class Task3 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        while (a < b){
+            if (a % 5 == 0 && a % 10 != 0){
+                System.out.print(a+" ");
+
+            }
+            a++;
+        }
 
     }
 }
